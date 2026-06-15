6bcac2741b0714aeb7714c6e84ac91400aeff060
Sarah Tasnim Diya <sarahtasnim99@gmail.com>
2026-06-16 05:18:42 +0600

    changed to http

--- server/email.js
+++ server/email.js
@@ -1,35 +1,22 @@
-const nodemailer = require('nodemailer');
+const brevo = require('@getbrevo/brevo');
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+const apiInstance = new brevo.TransactionalEmailsApi();
+apiInstance.setApiKey(
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.EMAIL_PASS
+);
 
-
-tls: {
-    rejectUnauthorized: false,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 15000,
-});
-
-transporter.verify((error) => {
-  if (error) console.error('Email transporter error:', error);
-  else console.log('Email server ready');
-});
 
 async function sendVerificationEmail(toEmail, token) {
   const link = `${process.env.SERVER_URL}/api/auth/verify/${token}`;
 
     try {
-    const info = await transporter.sendMail({
-    from: `"User Manager" <${process.env.EMAIL_USER}>`,
-    to: toEmail,
+    await apiInstance.sendTransacEmail({
+      sender: {
+        email: 'sarahtasnim99@gmail.com',
+        name: 'The App: User Manager'
+      },
+    to: [{ email: toEmail }],
     subject: 'Verify your e-mail',
     html: `
       <h2>Welcome!</h2>
